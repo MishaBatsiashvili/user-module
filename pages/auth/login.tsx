aabdd131cea31f54cf0f33a7fb7adec2cb2e0f9b
Misha batsiashvili <mishabatsiashvili@yahoo.com>
2022-11-03 01:28:12 +0400

    updated login page with new icons and common components

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,86 +1,87 @@
 import React from 'react';
 import Image from 'next/image';
-import bog from '../../public/assets/bog.png';
-import fb from '../../public/assets/fb.png';
 import google from '../../public/assets/google.png';
 import Link from 'next/link';
+import FacebookSvgIcon from '../../components/common/svg/FacebookSvgIcon';
+import BogSvgIcon from '../../components/common/svg/BogSvgIcon';
+import TextHr from '../../components/common/TextHr';
 
 export function login() {
+   const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
+      // Preventing the page from reloading
+      event.preventDefault();
+      // Do something
+      console.log('submited');
+   };
 
-    const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
-        // Preventing the page from reloading
-        event.preventDefault();
-        // Do something
-        console.log('submited');
-    };
-
-    return (
-        <div className="app">
-            <div className="auth-form" >
-                <div>
-                    <div className='text-4xl text-center pb-5'>Log in</div>
-                </div>
-
-                <form onSubmit={submitForm}>
-                    <input type="Email" placeholder="Email" className="input input-bordered w-full max-w-xs" />
-                    <input type="Password" placeholder="Password" className="input input-bordered w-full max-w-xs" />
-                    <button className="btn btn-primary btn-wide">Log In</button>
-                </form>
-
-                <div className="text-center my-2"><a className="link link-primary mt-5">Forgot Your Password?</a></div>
-                <div className="relative my-8">
-                    <div className="bg-black h-0.5">
-                    </div>
-                    <div className="hr-line">  or continue with  </div>
-                </div>
-
-                <div>
-                    <button className="btn btn-outline hover:text-white text-google hover:bg-google">
-                        <div className='auth-image-div'>
-                            <Image
-                                src={google}
-                                alt="Picture of the author"
-                                width={30}
-                                height={30}
-                            />
-                        </div>
-                        google
-                    </button>
-
-                    <button className="btn btn-outline hover:text-white text-fb hover:bg-fb">
-                        <div className='auth-image-div'>
-                            <Image
-                                src={fb}
-                                alt="Picture of the author"
-                                width={30}
-                                height={30}
-                            />
-                        </div>
-                        fb
-                    </button>
-
-                    <button className="btn btn-outline group hover:text-white text-bog hover:bg-bog">
-                        <div className='auth-image-div'>
-                            {/* image ზე ჰოვერი მუშაობს როცა სურათთან მიგვაქვს მაუსი */}
-                            <Image
-                                className='group-hover:bg-white group-hover:rounded'
-                                src={bog}
-                                alt="Picture of the author"
-                                width={30}
-                                height={30}
-                            />
-                        </div>
-                        bog
-                    </button>
-                </div>
-
-                <div className="relative my-8">
-                    <div className="bg-black h-0.5">
-                    </div>
-                    <div className="hr-line">  <div className="text-center my-2">New user? <Link href="/auth/register"><a className="link link-primary">Register now </a></Link></div> </div>
-                </div>
+   return (
+      <div className='app'>
+         <div className='auth-form'>
+            <div>
+               <div className='pb-5 text-center text-4xl'>Log in</div>
             </div>
-        </div>
-    );
+
+            <form onSubmit={submitForm}>
+               <input
+                  type='Email'
+                  placeholder='Email'
+                  className='input-bordered input w-full max-w-xs'
+               />
+               <input
+                  type='Password'
+                  placeholder='Password'
+                  className='input-bordered input w-full max-w-xs'
+               />
+               <button className='btn-primary btn-wide btn'>Log In</button>
+            </form>
+
+            <div className='my-2 text-center'>
+               <a href='#' className='link-primary mt-5'>Forgot Your Password?</a>
+            </div>
+            <div className='relative my-8'>
+               <TextHr>or continue with</TextHr>
+            </div>
+
+            <div>
+               <button className='btn-secondary btn'>
+                  <div className='auth-image-div'>
+                     <Image
+                        src={google}
+                        alt='Picture of the author'
+                        width={30}
+                        height={30}
+                     />
+                  </div>
+                  Google
+               </button>
+
+               <button className='btn bg-fb text-white hover:bg-fb-hover hover:text-white'>
+                  <div className='auth-image-div'>
+                     <FacebookSvgIcon />
+                  </div>
+                  Facebook
+               </button>
+
+               <button className='group btn  bg-bog text-white hover:bg-bog-hover hover:text-white'>
+                  <div className='auth-image-div'>
+                     <BogSvgIcon />
+                  </div>
+                  Bank of Georgia
+               </button>
+            </div>
+
+            <div className='relative my-8'>
+               <TextHr>
+                  <div className='my-2 text-center'>
+                     New user?{' '}
+                     <Link href='/auth/register'>
+                        <a href='#' className='link-primary'>Register now </a>
+                     </Link>
+                  </div>
+               </TextHr>
+            </div>
+         </div>
+      </div>
+   );
 }
 export default login;
